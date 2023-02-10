252e996dbb44d2e5ae80085b4eb43a7ac8388764
Edson.Talent <edson.talent@fleetcor.com.br>
2023-02-10 10:09:11 -0300

    Changed background color in Main

--- src/atomic/atoms/main/style.tsx
+++ src/atomic/atoms/main/style.tsx
@@ -4,5 +4,5 @@ import { IndexStyledProps } from "./models";
 export const Container = styled.ScrollView`
     flex: 1;
     padding: ${( props: IndexStyledProps ) => ( props.padding ? `20px 16px` : '0px' )};
-    background-color: ${( props: IndexStyledProps ) => ( props.bgColor ?? 'transparent' )};
+    background-color: ${( props: IndexStyledProps ) => ( props.bgColor ?? '#FFF' )};
 `
