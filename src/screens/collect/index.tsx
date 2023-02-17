1e4d371444a6d850671de8c4fb2f624b91e3c672
Edson.Talent <edson.talent@fleetcor.com.br>
2023-02-17 15:13:21 -0300

    Added Printer component

--- src/screens/collect/index.tsx
+++ src/screens/collect/index.tsx
@@ -1,4 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+
+import { Alert } from "react-native";
+import { BluetoothManager, BluetoothEscposPrinter } from "tp-react-native-bluetooth-printer";
 
 import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
@@ -33,7 +36,7 @@ const Items: ItemType[] = [
     },
     {
         id: 2,
-        description: 'Resíduos Abate',
+        description: 'Residuos Abate',
         prevision: '400',
         value: '',
     },
@@ -58,7 +61,135 @@ const Collect: React.FC = () => {
     const [showModal, setShowModal] = useState<boolean>(false)
     const [items, setItems] = useState<ItemType[]>(Items)
 
-    const confirm = () => {
+    useEffect(() => {
+        connect()
+    }, [])
+
+    const connect = () => {
+        BluetoothManager.enableBluetooth().then(
+            async (r) => {
+                var paired = [];
+                if (r && r.length > 0) {
+                    for (var i = 0; i < r.length; i++) {
+                    try {
+                        paired.push(JSON.parse(r[i]));
+                    } catch (e) {
+                        Alert.alert('Paired Printers', e)
+                    }
+                    }
+                }
+
+                await BluetoothManager.connect(paired[0].address)
+            },
+            (err) => Alert.alert('Enable Bluetooth', err)
+        );
+    }
+
+    const confirm = async () => {
+        await BluetoothEscposPrinter.printText('--------------------------------', {
+            encoding: "Cp857",
+            codepage: 13,
+            fonttype: 0,
+            widthtimes: 3,
+            heigthtimes: 3,
+        });
+        await BluetoothEscposPrinter.printText(`${('NUTRIFORTE').padStart(21, ' ')}`, {
+            encoding: "Cp857",
+            codepage: 13,
+            fonttype: 0,
+            widthtimes: 3,
+            heigthtimes: 3,
+        });
+        await BluetoothEscposPrinter.printText(`${('(61) 3626-0000').padStart(23, ' ')}`, {
+            encoding: "Cp857",
+            codepage: 13,
+            fonttype: 1,
+            widthtimes: 3,
+            heigthtimes: 3,
+        });
+        await BluetoothEscposPrinter.printText('--------------------------------', {
+            encoding: "Cp857",
+            codepage: 13,
+            fonttype: 0,
+            widthtimes: 3,
+            heigthtimes: 3,
+        });
+        await BluetoothEscposPrinter.printText('Código: 86822', {
+            encoding: "Cp857",
+            codepage: 13,
+            fonttype: 1,
+            widthtimes: 3,
+            heigthtimes: 3,
+        });
+        let date = new Date();
+        let currentDate = `${date.getDate()}/${String(date.getMonth()+1).padStart(2, '0')}/${date.getFullYear()}`
+        let currentTime = `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}:${String(date.getSeconds()).padStart(2, '0')}`
+
+        await BluetoothEscposPrinter.printText(`Data: ${currentDate} ${currentTime}`, {
+            encoding: "Cp857",
+            codepage: 13,
+            fonttype: 1,
+            widthtimes: 3,
+            heigthtimes: 3,
+        });
+        await BluetoothEscposPrinter.printText('12-TATICO / REC DAS EMAS', {
+            encoding: "Cp857",
+            codepage: 13,
+            fonttype: 1,
+            widthtimes: 3,
+            heigthtimes: 3,
+        });
+        await BluetoothEscposPrinter.printText('================================', {
+            encoding: "Cp857",
+            codepage: 13,
+            fonttype: 0,
+            widthtimes: 3,
+            heigthtimes: 3,
+        });
+        await BluetoothEscposPrinter.printText('Produto               Quantidade', {
+            encoding: "Cp857",
+            codepage: 13,
+            fonttype: 1,
+            widthtimes: 3,
+            heigthtimes: 3,
+        });
+        await BluetoothEscposPrinter.printText('================================', {
+            encoding: "Cp857",
+            codepage: 13,
+            fonttype: 0,
+            widthtimes: 3,
+            heigthtimes: 3,
+        });
+
+        for (let i = 0; i < items.length; i++) {
+            if (Number(items[i].value) > 0) {
+                await BluetoothEscposPrinter.printText(`${items[i].description.padEnd(31 - items[i].value.length, ' ')} ${items[i].value}`, {
+                    encoding: "Cp857",
+                    codepage: 13,
+                    fonttype: 1,
+                    widthtimes: 3,
+                    heigthtimes: 3,
+                });
+            }
+        }
+
+        await BluetoothEscposPrinter.printText('================================', {
+            encoding: "Cp857",
+            codepage: 13,
+            fonttype: 0,
+            widthtimes: 3,
+            heigthtimes: 3,
+        });
+        let total = currency(items.reduce((valor, total) => Number(valor) + Number(total.value), 0), 2, 3, '.', ',');
+        await BluetoothEscposPrinter.printText(total.padStart(32, ' '), {
+            encoding: "Cp857",
+            codepage: 13,
+            fonttype: 1,
+            widthtimes: 3,
+            heigthtimes: 3,
+        });
+        await BluetoothEscposPrinter.printText('\r\n\r\n\r\n', {});
+
         setShowModal(false)
         navigation.navigate('Success')
     }
