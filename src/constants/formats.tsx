e335d95e56cea2d7817c04c418e45d275e6444aa
Edson.Talent <edson.talent@fleetcor.com.br>
2023-06-15 11:01:35 -0300

    feat: new format boxID

--- src/constants/formats.tsx
+++ src/constants/formats.tsx
@@ -4,3 +4,12 @@ export function currency(value: number, n: number, x: number, s: string, c: stri
 
     return (c ? num.replace('.', c) : num).replace(new RegExp(re, 'g'), '$&' + (s || ','));
 };
+
+export function boxID(value: string): string {
+    let format = value.split(' ')
+    let char0 = format[0].substring(0, 1);
+    let char1 = format[1].substring(0, 1)
+    if (format[1] == 'DA' || format[1] == 'DE')
+        char1 = format[2].substring(0, 1)
+    return `${char0}${char1}`
+}
