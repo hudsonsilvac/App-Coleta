38aadc3dff51ebd7f99cdb14f66147844f48ccd7
Edson.Talent <edson.talent@fleetcor.com.br>
2023-06-15 18:35:50 -0300

    feat: added redux

--- App.tsx
+++ App.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 
+import './src/services/reactotron'
 import Routes from './src/routes'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/es/integration/react';
+import { persistor, store } from './src/services/redux';
 
 export default function App() {
   return (
-    <Routes />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Routes />
+      </PersistGate>
+    </Provider>
   )
 }
